cfce8ef4a35e1f4ffc3f3b25d81e4bf77accea64
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-05-19 21:56:16 +0300

    init

---
+++ src/main/java/mg/working/allinstore/AllInStoreApplication.java
@@ -0,0 +1,13 @@
+package mg.working.allinstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/working/allinstore/AllInStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.working.allinstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllInStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
